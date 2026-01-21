e35c23071dd0983ae2f52fe63b28ad93e2664da5
GuillemotClement <guillemot.clement@protonmail.com>
2026-01-21 21:07:42 +0100

    feat: add base projet from ubklub

---
+++ front/src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header/Header.tsx";
+import {Outlet} from "@tanstack/react-router";
+import Footer from "./components/Footer/Footer.tsx";
+
+function App() {
+
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<div className="flex-1">
+				<Outlet />
+			</div>
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
