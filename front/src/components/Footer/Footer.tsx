9d7d065303ef06995dfafe4c87cdeb8695b592be
GuillemotClement <guillemot.clement@protonmail.com>
2026-01-23 20:04:05 +0100

    feat: update header and footer

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,3 +1,7 @@
-export default function Footer(){
-  return <h1>Footer</h1>
+export default function Footer() {
+	return (
+		<footer className="border-t border-gray-200 py-3 px-10">
+			<p>Copyright &copy; 2026 - Korrweb</p>
+		</footer>
+	);
 }
--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,25 +1,17 @@
-import {Link} from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 
-export default function Header(){
-  return <header className="flex items-center justify-between shadow py-4 px-5">
-    <div className="">
-      <p>UbRun</p>
-    </div>
+export default function Header() {
+	return (
+		<header className="flex items-center justify-between shadow py-4 px-10">
+			<Link to="/" className="">
+				<p>UbRun</p>
+			</Link>
 
-    <ul>
-      <li>
-        <Link to="/tools">Outils</Link>
-      </li>
-    </ul>
-
-
-    <div className="flex gap-x-3">
-      <li className="btn">
-        <Link to="/">Acceuil</Link>
-      </li>
-      <li className="btn">
-        <Link to="/tools">Outils</Link>
-      </li>
-    </div>
-  </header>
+			<ul>
+				<li>
+					<Link to="/tools">Outils</Link>
+				</li>
+			</ul>
+		</header>
+	);
 }
