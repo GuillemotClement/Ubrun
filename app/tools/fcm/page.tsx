e15d1bd38b3012229808be190031d5b945fdd9af
GuillemotClement <guillemot.clement@protonmail.com>
2026-05-05 21:20:46 +0200

    feat: add form value FCM

--- app/tools/fcm/page.tsx
+++ app/tools/fcm/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -30,8 +32,22 @@ const formAgeSchema = z.object({
   }),
 });
 
+const formValueSchema = z.object({
+  fcMax: z
+    .number()
+    .min(100, 'Une valeur valide est attendu')
+    .max(250, 'Une valeur valide est attendu')
+    .positive('Une valeur positive est attendu'),
+  fcRepos: z
+    .number()
+    .min(10, 'Une valeur valide est attendu')
+    .max(210, 'Une valeur valide est attendu')
+    .positive('Une valeur positive est attendu'),
+});
+
 // Définition des types du composants
 type FormAgeValue = z.infer<typeof formAgeSchema>;
+type FormValue = z.infer<typeof formValueSchema>;
 type TypeForm = 'age' | 'value';
 
 // option pour les checkboxs
@@ -47,6 +63,8 @@ const genderType = [
 ];
 
 export default function FcmPage() {
+  const [typeForm, setTypeForm] = useState<TypeForm>('age');
+
   const ageForm = useForm<FormAgeValue>({
     resolver: zodResolver(formAgeSchema),
     defaultValues: {
@@ -55,9 +73,21 @@ export default function FcmPage() {
     },
   });
 
+  const valueForm = useForm<FormValue>({
+    resolver: zodResolver(formValueSchema),
+    defaultValues: {
+      fcMax: undefined,
+      fcRepos: undefined,
+    },
+  });
+
   const onSubmitAge = (data: FormAgeValue) => {
     console.log(data);
     // TODO: aller chercher les données
+  };
+
+  const onSubmitValue = (data: FormValue) => {
+    console.log(data);
   };
 
   const handleReset = () => {
@@ -65,39 +95,70 @@ export default function FcmPage() {
       age: undefined,
       gender: undefined,
     });
+
+    valueForm.reset({
+      fcMax: undefined,
+      fcRepos: undefined,
+    });
   };
 
   return (
     <div className="">
       <Card className="">
         <CardHeader className="flex flex-col">
-          <div className="">
+          <div className="w-full">
             <CardTitle className="text-center">Fréquence Cardiaque</CardTitle>
-            <CardDescription className="text-justify my-2">
+            <CardDescription className="text-center my-2">
               Obtiens tes zones d’entraînement cardiaque à partir de ta fréquence cardiaque maximale
               théorique ou de tes valeurs physiologiques mesurées.
             </CardDescription>
           </div>
           <CardAction className="flex justify-center w-full gap-x-3">
-            <Button>Par âge</Button>
-            <Button>Par valeur</Button>
+            <Button onClick={() => setTypeForm('age')}>Par âge</Button>
+            <Button onClick={() => setTypeForm('value')}>Par valeur</Button>
           </CardAction>
         </CardHeader>
         <CardContent>
-          <Form onSubmit={ageForm.handleSubmit(onSubmitAge)} title="">
-            <FormInputNumber
-              min={1}
-              max={140}
-              name="age"
-              form={ageForm}
-              label="Age :"
-              placeholder=""
-              isRequired={true}
-              step={0}
-            />
-            <FormInputRadio form={ageForm} name="gender" types={genderType} label="Genre :" />
-            <FormAction submitText="Calculer" handleReset={handleReset} />
-          </Form>
+          {typeForm === 'age' ? (
+            <Form onSubmit={ageForm.handleSubmit(onSubmitAge)}>
+              <FormInputNumber
+                min={1}
+                max={140}
+                name="age"
+                form={ageForm}
+                label="Age :"
+                placeholder=""
+                isRequired={true}
+                step={0}
+              />
+              <FormInputRadio form={ageForm} name="gender" types={genderType} label="Genre :" />
+              <FormAction submitText="Calculer" handleReset={handleReset} />
+            </Form>
+          ) : (
+            <Form onSubmit={valueForm.handleSubmit(onSubmitValue)}>
+              <FormInputNumber
+                min={100}
+                max={250}
+                name="fcMax"
+                form={valueForm}
+                label="FcMax :"
+                placeholder=""
+                isRequired={true}
+                step={0}
+              />
+              <FormInputNumber
+                min={10}
+                max={210}
+                name="fcRepos"
+                form={valueForm}
+                label="Fc Repos :"
+                placeholder=""
+                isRequired={true}
+                step={0}
+              />
+              <FormAction submitText="Calculer" handleReset={handleReset} />
+            </Form>
+          )}
         </CardContent>
       </Card>
 
