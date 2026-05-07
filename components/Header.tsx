02878344a807ba0cdbc27cd0fcf6991471fd15a1
GuillemotClement <guillemot.clement@protonmail.com>
2026-05-07 23:57:15 +0200

    feat: add logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,29 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import { authClient } from '@/lib/betterAuth/auth-client';
 
 import NavLink from './NavLink';
 import { Button } from './ui/button';
 
 export default function Header() {
+  const { data: session } = authClient.useSession();
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/');
+        },
+      },
+    });
+  };
+
   return (
     <header className="shadow flex justify-between px-10 py-4">
       <div className="flex items-center">
@@ -18,14 +37,21 @@ export default function Header() {
           <NavLink href="/tools/convertor">Convertisseur</NavLink>
         </div>
       </div>
-      <div className="">
-        <Link href="/auth/register">
-          <Button>Inscription</Button>
-        </Link>
-        <Link href="/auth/login">
-          <Button>Connexion</Button>
-        </Link>
-      </div>
+      {!session ? (
+        <div className="">
+          <Link href="/auth/register">
+            <Button>Inscription</Button>
+          </Link>
+          <Link href="/auth/login">
+            <Button>Connexion</Button>
+          </Link>
+        </div>
+      ) : (
+        <div className="">
+          <NavLink href="/profil">{session.user.name}</NavLink>
+          <Button onClick={handleLogout}>Logout</Button>
+        </div>
+      )}
     </header>
   );
 }
